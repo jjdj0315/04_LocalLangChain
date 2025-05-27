f0500b6c572e1ed838453da6fc497677251d0526
jjdj0315 <jjdj0315@gmail.com>
2025-05-27 15:59:00 +0900

    study : finish 03prac

--- 03prac/app.py
+++ 03prac/app.py
@@ -4,15 +4,50 @@ from utils.session import session_control
 from utils.create_dir import create_dir
 from utils.upload import embed_file
 from utils.create_rag_chain import create_rag_chain
+from utils.print_message import print_messages
+from utils.add_message import add_message
 
 session_control()
 create_dir()
+# 페이지 설정
+st.set_page_config(page_title="100% 오픈모델 RAG", page_icon="💬")
+st.title("100% 오픈모델 RAG")
 
-st.set_page_config(page_title="100% 오픈모델 RAG")
-st.title("100%오픈모델RAG")
 
 with st.sidebar:
-    file = st.file_uploader("파일 업로드", type=["pdf"])
+    file = st.file_uploader(
+        "파일 업로드",
+        type=["pdf"],
+    )
     if file:
         file_path = embed_file(file)
         rag_chain = create_rag_chain(file_path)
+        st.session_state["chain"] = rag_chain
+
+# 메시지 출력
+print_messages()
+
+
+if user_input := st.chat_input():
+
+    if "chain" in st.session_state and st.session_state["chain"] is not None:
+        chain = st.session_state["chain"]
+        # 사용자의 입력
+        st.chat_message("user").write(user_input)
+
+        # 스트리밍 호출
+        response = chain.stream(user_input)
+        with st.chat_message("assistant"):
+            # 빈 공간(컨테이너)을 만들어서, 여기에 토큰을 스트리밍 출력한다.
+            container = st.empty()
+
+            ai_answer = ""
+            for token in response:
+                ai_answer += token
+                container.markdown(ai_answer)
+
+        # 대화기록을 저장한다.
+        add_message("user", user_input)
+        add_message("assistant", ai_answer)
+    else:
+        st.write("파일을 업로드해주세요.")
--- 03prac/utils/create_rag_chain.py
+++ 03prac/utils/create_rag_chain.py
@@ -12,39 +12,46 @@ from langchain_core.output_parsers import StrOutputParser
 from utils.format_docs import format_docs
 from utils.add_message import add_message
 
+
+# RAG 체인 생성
 @st.cache_resource(show_spinner="파일을 처리중입니다. 잠시만 기다려주세요.")
 def create_rag_chain(file_path):
-    #spliter설정
-    text_spliter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-    
-    #문서로드
+    # Splitter 설정
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+
+    # 문서 로드
     loader = PDFPlumberLoader(file_path)
-    docs = loader.load_and_split(text_splitter= text_spliter)
-    
-    #캐싱지원 임베딩설정
+    docs = loader.load_and_split(text_splitter=text_splitter)
+
+    # 캐싱을 지원하는 임베딩 설정
     cache_dir = LocalFileStore(f".cache/embeddings")
     EMBEDDING_MODEL = "bge-m3"
-    embeddings = OllamaEmbeddings(model = EMBEDDING_MODEL)
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir, namespace = EMBEDDING_MODEL)
-    
-    #벡터 DB 저장
-    vectorstore = FAISS.from_documents(docs, embedding = cached_embeddings)
-    
-    #문서 검색기 설정
-    retriver = vectorstore.as_retriever()
-    
-    #프롬프트 로드
+    embeddings = OllamaEmbeddings(model=EMBEDDING_MODEL)
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings, cache_dir, namespace=EMBEDDING_MODEL
+    )
+
+    # 벡터 DB 저장
+    vectorstore = FAISS.from_documents(docs, embedding=cached_embeddings)
+
+    # 문서 검색기 설정
+    retriever = vectorstore.as_retriever()
+
+    # 프롬프트 로드
     prompt = load_prompt("utils/prompts/rag-exaone.yaml", encoding="utf-8")
 
-    #Ollama모델 지정
-    llm = ChatOllama(model = "mistral", temperature=0.0)
+    # Ollama 모델 지정
+    llm = ChatOllama(
+        model="mistral",
+        temperature=0,
+    )
 
-    #체인생성
+    # 체인 생성
     chain = (
-        {"context": retriver | format_docs, "quertion" : RunnablePassthrough()}
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
         | llm
         | StrOutputParser()
     )
-    
-    add_message("assistant", "준비가 완료되었습니다.")
+    add_message("assistant", "준비가 완료되었습니다. 무엇을 도와드릴까요?")
+    return chain
